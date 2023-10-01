b9fe4c2e3fdfcfbfbe15935c62b6dff56c320a8c
ALAN PEÑIÑOREY <gapgonza@gmail.com>
2023-10-01 20:07:16 -0300

    .

--- validacion.js
+++ validacion.js
@@ -1,14 +1,3 @@
 /*----------------------Carrusel de imagenes------------------------*/
-const carrusel = document.querySelector('.carrusel-items');
-let intervalo = null;
-
-const start = () => {
-  intervalo = setInterval(function(params){
-    carrusel.scrollLeft = carrusel.scrollLeft + 20;
-  },10);
-};
-
-const stop = () => {};
-
-start();
+var
 /*---------------Validaciones Formulario---------------------*/
