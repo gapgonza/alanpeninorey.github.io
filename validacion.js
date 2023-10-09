ec5d612cc957a55bb7906437ad4db45e9ae76b21
ALAN PEÑIÑOREY <gapgonza@gmail.com>
2023-10-08 21:55:49 -0300

    .

--- validacion.js
+++ validacion.js
@@ -1,22 +1,26 @@
 /*----------------------Carrusel de imagenes------------------------*/
- // JavaScript para el carrusel
- const carruselItems = document.querySelector('.carrusel-items');
- const carruselButtonAnterior = document.getElementById('anterior');
- const carruselButtonSiguiente = document.getElementById('siguiente');
- const carruselItemWidth = document.querySelector('.carrusel-item').clientWidth;
- let currentPosition = 0;
+    const carruselItems = document.querySelector('.carrusel-items');
+    const carruselButtonAnterior = document.getElementById('anterior');
+    const carruselButtonSiguiente = document.getElementById('siguiente');
+    const carruselItemWidth = document.querySelector('.carrusel-item').clientWidth;
+    const numItems = document.querySelectorAll('.carrusel-item').length;
+    let currentPosition = 0;
 
- carruselButtonAnterior.addEventListener('click', () => {
-     if (currentPosition > 0) {
-         currentPosition -= carruselItemWidth;
-         carruselItems.style.transform = `translateX(-${currentPosition}px)`;
-     }
- });
+    carruselButtonAnterior.addEventListener('click', () => {
+        if (currentPosition > 0) {
+            currentPosition -= carruselItemWidth;
+        } else {
+            currentPosition = (numItems - 1) * carruselItemWidth;
+        }
+        carruselItems.style.transform = `translateX(-${currentPosition}px)`;
+    });
 
- carruselButtonSiguiente.addEventListener('click', () => {
-     const numItems = document.querySelectorAll('.carrusel-item').length;
-     if (currentPosition < (numItems - 1) * carruselItemWidth) {
-         currentPosition += carruselItemWidth;
-         carruselItems.style.transform = `translateX(-${currentPosition}px)`;
-     }
- });
+    carruselButtonSiguiente.addEventListener('click', () => {
+        if (currentPosition < (numItems - 1) * carruselItemWidth) {
+            currentPosition += carruselItemWidth;
+        } else {
+            currentPosition = 0;
+        }
+        carruselItems.style.transform = `translateX(-${currentPosition}px)`;
+    });
+
