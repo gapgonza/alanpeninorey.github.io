b28b4b340a8049b43b75e55ab92d6f22de545a3a
ALAN PEÑIÑOREY <gapgonza@gmail.com>
2023-10-08 22:02:30 -0300

    documentacion js
    Se documenta las funciones y las variables para entender que hago

--- validacion.js
+++ validacion.js
@@ -1,17 +1,23 @@
 /*----------------------Carrusel de imagenes------------------------*/
-    const carruselItems = document.querySelector('.carrusel-items');
-    const carruselButtonAnterior = document.getElementById('anterior');
-    const carruselButtonSiguiente = document.getElementById('siguiente');
+    const carruselItems = document.querySelector('.carrusel-items');//almacenamos los elementos
+    const carruselButtonAnterior = document.getElementById('anterior');//boton anterior
+    const carruselButtonSiguiente = document.getElementById('siguiente'); //boton siguiente
+    //fija el ancho del elemento
     const carruselItemWidth = document.querySelector('.carrusel-item').clientWidth;
+    //totla los elementos
     const numItems = document.querySelectorAll('.carrusel-item').length;
-    let currentPosition = 0;
+    let currentPosition = 0; //posicion actual del carrusel
 
+    //estas funciones controlan los eventos de los botones
     carruselButtonAnterior.addEventListener('click', () => {
         if (currentPosition > 0) {
+            //si no estamos en la primer imagen volvemos
             currentPosition -= carruselItemWidth;
         } else {
+            //lo mismo si estamo s en la primera, vamos a la ultima para girar
             currentPosition = (numItems - 1) * carruselItemWidth;
         }
+        //una especie de transformacion para mover
         carruselItems.style.transform = `translateX(-${currentPosition}px)`;
     });
 
@@ -23,4 +29,6 @@
         }
         carruselItems.style.transform = `translateX(-${currentPosition}px)`;
     });
+    /*-----------------------------Validacion formulario-------------------------------*/
+
 
