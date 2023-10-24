68594c0233d89a7362808ebb8abea8662266654b
ALAN PEÑIÑOREY <134467539+gapgonza@users.noreply.github.com>
2023-10-24 16:09:55 -0300

    .

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,34 +1,3 @@
-// const carruselItems = document.querySelector('.carrusel-items');//almacenamos los elementos
-//     const carruselButtonAnterior = document.getElementById('anterior');//boton anterior
-//     const carruselButtonSiguiente = document.getElementById('siguiente'); //boton siguiente
-//     //fija el ancho del elemento
-//     const carruselItemWidth = document.querySelector('.carrusel-item').clientWidth;
-//     //totla los elementos
-//     const numItems = document.querySelectorAll('.carrusel-item').length;
-//     let currentPosition = 0; //posicion actual del carrusel
-
-//     //estas funciones controlan los eventos de los botones
-//     carruselButtonAnterior.addEventListener('click', () => {
-//         if (currentPosition > 0) {
-//             //si no estamos en la primer imagen volvemos
-//             currentPosition -= carruselItemWidth;
-//         } else {
-//             //lo mismo si estamo s en la primera, vamos a la ultima para girar
-//             currentPosition = (numItems - 1) * carruselItemWidth;
-//         }
-//         //una especie de transformacion para mover
-//         carruselItems.style.transform = `translateX(-${currentPosition}px)`;
-//     });
-
-//     carruselButtonSiguiente.addEventListener('click', () => {
-//         if (currentPosition < (numItems - 1) * carruselItemWidth) {
-//             currentPosition += carruselItemWidth;
-//         } else {
-//             currentPosition = 0;
-//         }
-//         carruselItems.style.transform = `translateX(-${currentPosition}px)`;
-//     });
-
 const imagenes = [
     'images/Carga de Productos.png',
     'images/Empresa-Empleados1.png',
